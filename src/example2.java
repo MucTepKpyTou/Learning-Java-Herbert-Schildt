7c30fc643e2b615c4019ba7d3852f2076e4049de
Эдуард <muctepkpytou@mail.ru>
2020-03-16 02:37:55 +0300

    2 первые проги

---
+++ src/example2.java
@@ -0,0 +1,12 @@
+public class example2 {
+    public static void main(String[] args) {
+        int var1, var2;
+        var1 = 1024;
+
+        System.out.println("var1 сожержит" + var1);
+        var2 = var1 / 2;
+        System.out.print("var2 сожержит var1/2: ");
+        System.out.println(var2);
+
+    }
+}
