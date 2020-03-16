256ae2d545c92dd1b130c18de2de534c2d160f7d
Эдуард <muctepkpytou@mail.ru>
2020-03-16 03:53:24 +0300

    3 прога

---
+++ src/example3.java
@@ -0,0 +1,14 @@
+public class example3 {
+    public static void main(String[] args) {
+        int var = 10;
+        double x = 10.0;
+        System.out.println("Начальное значение var:" + var);
+        System.out.println("Начальное значение x:" + x);
+        System.out.println();
+        var = var / 4;
+        x = x / 4;
+        System.out.println("Значение после деления var:" + var);
+        System.out.println("Значение после деления x:" + x);
+
+    }
+}
