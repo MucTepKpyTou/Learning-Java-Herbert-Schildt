8477a38ee52fc0515eae2d7d4de60cc025a27a60
EDUARD <59347174+MucTepKpyTou@users.noreply.github.com>
2020-03-16 20:24:00 +0300

    добавил для if { }

--- src/IfDemo.java
+++ src/IfDemo.java
@@ -1,23 +1,35 @@
 public class IfDemo {
     public static void main(String[] args) {
-        int a,b,c;
-        a=2;
-        b=3;
+        int a, b, c;
+        a = 2;
+        b = 3;
 
-        if (a < b) System.out.println(" а меньше б ");   // следующую строку вы увидеть не должны
-        if (b == a) System.out.println(" этот текст не будет выведен");  //вот эту...
+        if (a < b) {
+            System.out.println(" а меньше б ");   // следующую строку вы увидеть не должны
+        }
+        if (b == a) {
+            System.out.println(" этот текст не будет выведен");  //вот эту...
+        }
         System.out.println();
 
         c = a - b; // с приняло значение -1
         System.out.println("c равняется -1 ");
-        if (c >= 0) System.out.println("c не отрицательное число");
-        if (c < 0) System.out.println("c отрицательное число");
+        if (c >= 0) {
+            System.out.println("c не отрицательное число");
+        }
+        if (c < 0) {
+            System.out.println("c отрицательное число");
+        }
         System.out.println();
 
         c = b - a; //-1=2-3
         System.out.println("c равняется 1 ");
-        if (c >= 0) System.out.println("c  - не  отрицательное число");
-        if (c < 0) System.out.println("c - отрицательное число");
+        if (c >= 0) {
+            System.out.println("c  - не  отрицательное число");
+        }
+        if (c < 0) {
+            System.out.println("c - отрицательное число");
+        }
 
     }
 }
