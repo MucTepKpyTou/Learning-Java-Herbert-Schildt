e81bfa7a35b1a2aef06ae162e82c0042281eb47b
Эдуард <muctepkpytou@mail.ru>
2020-03-16 04:12:55 +0300

    4 прога

---
+++ src/GalToLit.java
@@ -0,0 +1,9 @@
+public class GalToLit {
+    public static void main(String[] args) {
+        double gallons, liters;
+        gallons = 10;
+        liters = gallons * 3.7854; //перевод в литры
+        System.out.println(gallons + " галлонам соответствует " + liters + " литров");
+
+    }
+}
