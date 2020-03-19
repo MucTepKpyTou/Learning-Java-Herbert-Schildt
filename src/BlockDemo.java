c8a63dbc47d2b13f7e6315705d88d90da82ee6b6
EDUARD <59347174+MucTepKpyTou@users.noreply.github.com>
2020-03-19 13:02:37 +0300

    7 прога BlockDemo

---
+++ src/BlockDemo.java
@@ -0,0 +1,13 @@
+public class BlockDemo {
+    public static void main(String[] args) {
+        double i, j, d;
+        i = 5;
+        j = 10;
+
+        if (i != 0) {
+            System.out.println(" i не равно 0");
+            d = j / i;
+            System.out.print(" j/i равно " + d);
+        }
+    }
+}
--- src/ForDemo.java
+++ src/ForDemo.java
@@ -1,8 +1,8 @@
 public class ForDemo {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         int count;
-        for(count=0; count<5; count=count + 1)
-            System.out.println("Значние count: " + count );
+        for (count = 0; count < 5; count = count + 1)
+            System.out.println("Значние count: " + count);
         System.out.println("Готово");
     }
 }
