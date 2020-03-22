84a83603ca4762bdbaf0d739abe2ffa80034a5c6
Эдуард <muctepkpytou@mail.ru>
2020-03-22 04:05:10 +0300

    Добавил в прогу Inches  ctrl+alt+l

--- src/Inches.java
+++ src/Inches.java
@@ -1,8 +1,8 @@
 public class Inches {
     public static void main(String[] args) {
-        long ci,im;
-        im=5280*12;
-        ci=im*im*im;
+        long ci, im;
+        im = 5280 * 12;
+        ci = im * im * im;
         System.out.println(" В одной кубической миле находится :  " + ci + " Кубических дюймов");
 
     }
