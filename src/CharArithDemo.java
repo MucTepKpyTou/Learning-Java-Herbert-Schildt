a0bab5a742a712724e69972efb3947a26e11a9d4
Эдуард <muctepkpytou@mail.ru>
2020-03-24 00:59:39 +0300

    Добавил прогу CharArithDemo работа с символами

---
+++ src/CharArithDemo.java
@@ -0,0 +1,11 @@
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+        ch = 'X';
+        System.out.println("сh содержит " + ch);
+        ch++; //инкремент переменной +1
+        System.out.println("Теперь ch содержит " + ch);
+        ch = 90; // присвоить переменной значение Z
+        System.out.println("Теперь перменная ch содержит " + ch);
+    }
+}
