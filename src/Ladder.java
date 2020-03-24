20a0b029bee2205b8686b512cc145c32d6877c08
Эдуард <muctepkpytou@mail.ru>
2020-03-24 12:39:03 +0300

    Добавил прогу Ladder for и вложенная конструкция if else

---
+++ src/Ladder.java
@@ -0,0 +1,19 @@
+public class Ladder {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 6; x++) {
+            if (x == 1)
+                System.out.println(" x равно еденице");
+            else if (x == 2)
+                System.out.println(" x равно двум");
+            else if (x == 3)
+                System.out.println(" x равно трем");
+            else if (x == 4)
+                System.out.println(" x равно четырем");
+            else
+                //условие выполняемое по умолчанию.
+                System.out.println("Значение х находится вне диапозона 1-4");
+        }
+    }
+}
