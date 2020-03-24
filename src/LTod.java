6b2f279dd9546461410287d90a0100e32d7d63b5
Эдуард <muctepkpytou@mail.ru>
2020-03-24 04:08:04 +0300

    Добавил прогу LTod автоматическое приобразование long в duoble

---
+++ src/LTod.java
@@ -0,0 +1,9 @@
+public class LTod {
+    public static void main(String[] args) {
+        long L;
+        double D;
+        L = 100123285L;
+        D = L;
+        System.out.println(" L и D\n " + L + " " + D);
+    }
+}
